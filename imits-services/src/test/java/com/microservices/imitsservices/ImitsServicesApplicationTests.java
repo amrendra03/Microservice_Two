ce7b13c098db42d6231e94e11d29270ae40dd053
amrendra03 <amrendra9076893685@gmail.com>
2024-01-03 11:48:53 +0530

    Microservice2-practice-initiate

---
+++ imits-services/src/test/java/com/microservices/imitsservices/ImitsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.imitsservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImitsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
