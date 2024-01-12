8e7c5bafe565834fd0f630c3d78f9b97626c70a8
amrendra03 <amrendrayadav617@gmail.com>
2024-01-12 18:48:35 +0530

    CurrencyConversion-Service

---
+++ currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyConversion.java
@@ -0,0 +1,91 @@
+package com.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+public class CurrencyConversion {
+
+	private Long id;
+	private String currency_from;
+	private String currency_to;
+	private BigDecimal quantity;
+	private BigDecimal conversionMultiple;
+	private BigDecimal totalCalculatedAmount;
+	private String environment;
+	
+	public CurrencyConversion() {
+		
+	}
+	
+	public CurrencyConversion(Long id, String currency_from, String currency_to,BigDecimal quantity, BigDecimal conversionMultiple,
+			 BigDecimal totalCalculatedAmount, String environment) {
+		super();
+		this.id = id;
+		this.currency_from = currency_from;
+		this.currency_to = currency_to;
+		this.conversionMultiple = conversionMultiple;
+		this.quantity = quantity;
+		this.totalCalculatedAmount = totalCalculatedAmount;
+		this.environment = environment;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCurrency_from() {
+		return currency_from;
+	}
+
+	public void setCurrency_from(String currency_from) {
+		this.currency_from = currency_from;
+	}
+
+	public String getCurrency_to() {
+		return currency_to;
+	}
+
+	public void setCurrency_to(String currency_to) {
+		this.currency_to = currency_to;
+	}
+
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+
+	public void setConversionMultiple(BigDecimal conversionMultiple) {
+		this.conversionMultiple = conversionMultiple;
+	}
+
+	public BigDecimal getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(BigDecimal quantity) {
+		this.quantity = quantity;
+	}
+
+	public BigDecimal getTotalCalculatedAmount() {
+		return totalCalculatedAmount;
+	}
+
+	public void setTotalCalculatedAmount(BigDecimal totalCalculatedAmount) {
+		this.totalCalculatedAmount = totalCalculatedAmount;
+	}
+
+	public String getEnvironment() {
+		return environment;
+	}
+
+	public void setEnvironment(String environment) {
+		this.environment = environment;
+	}
+	
+	
+	
+	
+	
+}
